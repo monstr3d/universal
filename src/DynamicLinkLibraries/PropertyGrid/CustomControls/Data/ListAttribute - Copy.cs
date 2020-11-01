6c746c1b3abfff1035f10b9f1c07019686845c22
monstr3d <monster.ivankov@gmail.com>
2020-11-01 14:33:56 +0300

    Delete Copy

--- src/DynamicLinkLibraries/PropertyGrid/CustomControls/Data/ListAttribute - Copy.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CustomControls.Data
-{
-	[AttributeUsage( AttributeTargets.Field | AttributeTargets.Property )]
-	public abstract class ListAttribute : Attribute
-	{
-	}
-}
