08a541df7da2a75ede9551870c6b13741a144a4a
monstr3d <monster.ivankov@gmail.com>
2022-12-30 12:48:52 +0300

    Tests NOT DEBUGGED

---
+++ src/DynamicLinkLibraries/DataPerformer/DataPerformer.TestInterface/DataPerformer.TestInterface/Regression/StruturedSeletionCollectionTest.cs
@@ -0,0 +1,142 @@
+﻿using DataPerformer.Interfaces;
+using Diagram.UI;
+using Diagram.UI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using TestCategory.Interfaces;
+
+namespace DataPerformer.TestInterface.Regression
+{
+    [Serializable]
+    public class StruturedSeletionCollectionTest : ITest, ISerializable
+    {
+        #region Fields
+
+        /// <summary>
+        /// Name of component on desktop
+        /// </summary>
+        protected string name;
+
+
+
+        /// <summary>
+        /// Residual parameter
+        /// </summary>
+        protected double[][] value;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of component on desktop</param>
+        public StruturedSeletionCollectionTest(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Streaming context</param>
+        protected StruturedSeletionCollectionTest(SerializationInfo info, StreamingContext context)
+        {
+            name = info.GetString("Name");
+            value = info.GetValue("Value", typeof(double[][])) as double[][];
+        }
+
+        #endregion
+
+        #region ISerializable Members
+
+        /// <summary>
+        /// ISerializable interface implementation
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Streaming context</param>
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", name);
+            info.AddValue("Value", value, typeof(double[][]));
+        }
+
+        #endregion
+
+        #region ITest Members
+
+        /// <summary>
+        /// Tests collection of components
+        /// </summary>
+        /// <param name="collection">Collection of components</param>
+        /// <returns>Test result</returns>
+        Tuple<bool, object> ITest.this[IComponentCollection collection]
+        {
+            get
+            {
+                var vt = GetValue(collection);
+                var n = vt.Length;
+                if (n != value.Length)
+                {
+                    return new Tuple<bool, object>(false, "Different selection values. Object - " + name); return new Tuple<bool, object>(false, "Different lengths. Object - " + name);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    var v = vt[i];
+                    var m = v.Length;
+                    var val = value[i];
+                    if (m != val.Length)
+                    {
+                        return new Tuple<bool, object>(false, "Different selection values. Object - " + name); return new Tuple<bool, object>(false, "Different lengths. Object - " + name);
+                    }
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (v[j] != val[j])
+                        {
+                            return new Tuple<bool, object>(false, "Different selection values. Object - " + name); return new Tuple<bool, object>(false, "Different lengths. Object - " + name);
+
+                        }
+                    }
+                }
+                return new Tuple<bool, object>(true, "Success. Object - " + name);        // Null means absence of error
+
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Calculates value of residual parameter
+        /// </summary>
+        /// <param name="collection">Collection of objects</param>
+        /// <returns>Residual parameter</returns>
+        protected double[][] GetValue(IComponentCollection collection)
+        {
+            var selections = collection.GetObject<IStructuredSelectionCollection>(name);
+            var n = selections.Count;
+            double[][] value = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                var selection = selections[i];
+                var m = selection.DataDimension;
+                value[i] = new double[m];
+                for (int j = 0; j < m; j++)
+                {
+                    value[i][j] = selection[j] ?? 0;
+                }
+            }
+            return value;
+        }
+        internal void Create(IComponentCollection collection)
+        {
+            value = GetValue(collection);
+        }
+
+    }
+}
---
+++ src/DynamicLinkLibraries/DataPerformer/DataPerformer.TestInterface/DataPerformer.TestInterface/TestData.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+using Diagram.UI.Interfaces;
+
+using TestCategory;
+using TestCategory.Interfaces;
+
+namespace DataPerformer.TestInterface
+{
+    [Serializable()]
+    public class TestData : TestCollection
+    {
+
+        protected List<ITest> testList = new List<ITest>();
+
+        #region Ctor
+
+        /// <summary>
+        /// Default
+        /// </summary>
+        public TestData() : base(null)
+        {
+            
+        }
+
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Streaming context</param>
+        protected TestData(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        #endregion
+
+        #region Members
+
+        /// <summary>
+        /// Output
+        /// </summary>
+        public Dictionary<string, object[]> Output
+        {
+            get
+            {
+                Dictionary<string, object[]> d = new Dictionary<string, object[]>();
+                for (int i = 0; i < tests.Length; i++)
+                {
+                    ITest test = tests[i];
+                    Process(i, test, d);
+                }
+                return d;
+            }
+        }
+
+
+        protected virtual bool Process(int i, ITest test, Dictionary<string, object[]> d)
+        {
+            switch (test)
+            {
+                case SeriesWrapper.LocalChart lc:
+                    d[lc.Name] = new object[] { i };
+                    return true;
+                case Regression.RegressionTest rt:
+                    d[rt.Name] = new object[] { i };
+                    return true;
+                case Regression.IteratorGLMTest it:
+                    d[it.Name] = new object[] { i };
+                    return true;
+                default:
+                    return false;
+           }
+  
+        }
+
+        public void AddChart(string name, double start, double step, 
+            int stepCount, string argument, string[] values, IComponentCollection collection)
+        {
+            SeriesWrapper.LocalChart lc = 
+                new SeriesWrapper.LocalChart(name, start, step, stepCount, argument, values);
+            lc.Create(collection);
+            testList.Add(lc);
+        }
+
+        public void AddRegression(string name, uint number, IComponentCollection collection)
+        {
+            Regression.RegressionTest rt = new Regression.RegressionTest(name, (int)number);
+            rt.Create(collection);
+            testList.Add(rt);
+        }
+
+        public void AddIteratorGLM(string name, uint number, IComponentCollection collection)
+        {
+            Regression.IteratorGLMTest it = new Regression.IteratorGLMTest(name, (int)number);
+            it.Create(collection);
+            testList.Add(it);
+        }
+
+        public void AddStructuredSelection(string name, IComponentCollection collection)
+        {
+            var it = new Regression.StruturedSeletionCollectionTest(name);
+            it.Create(collection);
+            testList.Add(it);
+        }
+
+
+
+        public void Close()
+        {
+            tests = testList.ToArray();
+            testList.Clear();
+        }
+
+        #endregion
+
+
+    }
+}
