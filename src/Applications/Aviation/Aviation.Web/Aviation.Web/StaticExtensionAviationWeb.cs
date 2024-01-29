57ca7fd17b59388c4fe4e2b8296c374b49c9063c
monstr3d <monster.ivankov@gmail.com>
2024-01-29 10:19:41 +0300

    Daily Update

---
+++ src/Applications/Aviation/Aviation.Web/Aviation.Web/StaticExtensionAviationWeb.cs
@@ -0,0 +1,15 @@
+﻿namespace Aviation.Web
+{
+    public static class StaticExtensionAviationWeb
+    {
+        static StaticExtensionAviationWeb()
+        {
+
+        }
+
+        public static void Init()
+        {
+
+        }
+    }
+}
