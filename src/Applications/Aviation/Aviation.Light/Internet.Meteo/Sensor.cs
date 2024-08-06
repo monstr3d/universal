00a7fb378466440d95f2e94669f01b12a762bb89
monstr3d <monster.ivankov@gmail.com>
2024-08-06 09:30:25 +0300

    Update

---
+++ src/Applications/Aviation/Aviation.Light/Internet.Meteo/Sensor.cs
@@ -0,0 +1,86 @@
+﻿namespace Internet.Meteo
+{
+    public class Sensor : IDisposable
+    {
+
+        #region Fields
+
+        double value;
+
+        object block = new object();
+
+        Task task;
+
+        Func<string, string> request;
+
+        #endregion
+
+        #region Public Members
+
+        public string Key { get; set; }
+
+        public string Posiition { get; set; }
+
+
+        public int Interval { get; set; }
+
+    
+        public double GetValue()
+        {
+            lock (block)
+            {
+                return value;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get => task != null;
+            set
+            {
+                if (value)
+                {
+                    if (task == null)
+                    {
+                        task = new Task(DoWork);
+                        task.Start();
+                    }
+                    return;
+                }
+                if (task != null)
+                {
+                    task.Dispose();
+                    task = null;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Ctor
+
+
+        #endregion
+
+
+
+        void IDisposable.Dispose()
+        {
+            KillTask();
+        }
+
+        void DoWork()
+        { 
+        
+        }
+
+        void KillTask()
+        {
+            if (task != null)
+            {
+                task.Dispose();
+            }
+        }
+
+    }
+}
