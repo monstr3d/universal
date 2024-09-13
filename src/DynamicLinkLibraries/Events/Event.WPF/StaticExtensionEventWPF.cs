b19cdcda65d31954b5265259934ce82bdee49fdb
monstr3d <monster.ivankov@gmail.com>
2024-09-13 16:56:22 +0300

    Backup

---
+++ src/DynamicLinkLibraries/Events/Event.WPF/StaticExtensionEventWPF.cs
@@ -0,0 +1,28 @@
+﻿using AssemblyService.Attributes;
+
+using Event.Interfaces;
+
+namespace Event.WPF
+{
+    /// <summary>
+    /// Static extension methods
+    /// </summary>
+    [InitAssembly]
+    public static class StaticExtensionEventWPF
+    {
+
+        /// <summary>
+        /// Inits itself
+        /// </summary>
+        static public void Init()
+        {
+
+        }
+
+        static StaticExtensionEventWPF()
+        {
+            StaticExtensionEventInterfaces.TimerEventFactory = WpfTimerEventFactory.Singleton;
+            StaticExtensionEventInterfaces.TimerFactory = WpfTimerFactory.Singleton;
+        }
+    }
+}
