b953311145a1b95e384e321c6f03424761a37991
monstr3d <monster.ivankov@gmail.com>
2024-11-16 09:37:09 +0300

    Update ObjToXamlConverter.cs

--- src/DynamicLinkLibraries/Collada/Collada.Wpf/ObjToXamlConverter.cs
+++ src/DynamicLinkLibraries/Collada/Collada.Wpf/ObjToXamlConverter.cs
@@ -55,24 +55,63 @@ namespace Collada.Wpf
         void Create(TextReader reader, string name = null)
         {
             string currName = name;
-            
+            List<float[]> vertices = new();
+            List<float[]> normals = new();
+            List<float[]> textures = new();
+
             ModelVisual3D modelVisual3D = new ModelVisual3D();
             while (true)
             {
                 var line = reader.ReadLine();
-                if (currName == null)
+                if (line == null)
                 {
-                    var objs = "# object ";
-                    if (line.Contains(objs))
+                    models[name] = modelVisual3D;
+                    break;
+                }
+                var objs = "# object ";
+                if (line.Contains(objs))
+                {
+                    var lt = line.Substring(objs.Length).Trim();
+                    if (currName == null)
                     {
-                        currName = line.Substring(objs.Length).Trim();
+                        currName = lt;
+                        if (name != null)
+                        {
+                            models[name] = modelVisual3D;
+                            Create(reader, currName);
+                        }
+
+                    }
+                    else
+                    {
+                        models[currName] = modelVisual3D;
+                        Create(reader, lt);
+
                     }
                 }
-                else
+                if (currName == null)
                 {
-                    models[currName] = modelVisual3D;
-                    
+                    continue;
                 }
+                if (line.IndexOf("v ") == 0)
+                {
+                    var f = line.Substring("v ".Length).Trim().ToRealArray<float>();
+                    vertices.Add(f);
+                    continue;
+                }
+                if (line.IndexOf("vn ") == 0)
+                {
+                    var f = line.Substring("vn ".Length).Trim().ToRealArray<float>();
+                    normals.Add(f);
+                    continue;
+                }
+                if (line.IndexOf("vt ") == 0)
+                {
+                    var f = line.Substring("vt ".Length).Trim().ToRealArray<float>();
+                    textures.Add(f);
+                    continue;
+                }
+                int i = 0;
             }
         }
 
