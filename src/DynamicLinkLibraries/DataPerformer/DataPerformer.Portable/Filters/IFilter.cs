ee1970ab631de01ddf673a1dc83287a505315a4c
monstr3d <monster.ivankov@gmail.com>
2026-09-23 14:38:44 +0300

    Cup product

--- src/DynamicLinkLibraries/DataPerformer/DataPerformer.Portable/Filters/IFilter.cs
+++ src/DynamicLinkLibraries/DataPerformer/DataPerformer.Portable/Filters/IFilter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataPerformer.Portable.Filters
+﻿namespace DataPerformer.Portable.Filters
 {
     public interface IFilter
     {
